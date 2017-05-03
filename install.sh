682582e19ab71edbfa413b2cb1bfd0ff38ec49fa
vianney <vianney@quasardb.net>
2017-05-03 12:53:25 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 git submodule init
 git submodule update
+# cp -R thirdparty/cpp-netlib/deps/asio/asio/include/asio* /usr/local/include/.
 mkdir -p build
 cd build
 cmake -DCPP-NETLIB_BUILD_SHARED_LIBS=OFF -DCPP-NETLIB_BUILD_TESTS=OFF -DCPP-NETLIB_BUILD_EXAMPLES=OFF -DCPP-NETLIB_ENABLE_HTTPS=OFF ..
